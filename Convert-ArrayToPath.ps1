7be834e7030a896bd619f1f16304c7c0f235dfe4
Tony Pagliaro <Tony@Pagliaro.co>
2021-08-23 15:54:49 -0400

    new function

---
+++ Convert-ArrayToPath.ps1
@@ -0,0 +1,45 @@
+function Convert-ArrayToPath {
+    <#
+    .SYNOPSIS
+    Join-Path for three or more strings.
+    .DESCRIPTION
+    Creates a path string joining all items with the path separator.
+    .EXAMPLE
+    @('c:\','temp','temp.txt') | Convert-ArrayToPath
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(
+            ValueFromPipeline,
+            Position=0
+        )]
+        [validatenotnull()]
+        [string[]]
+        $Array
+    )
+    
+    begin {
+        
+        [string]$cmdString = "[IO.Path]::Combine({0})"
+        [string]$arrParams = ''
+    }
+    
+    process {
+        
+        foreach ($string in $Array) {
+            
+            if ([string]::IsNullOrWhiteSpace($arrParams)) {} else {
+                $arrParams += ', '
+            }
+            $arrParams += "'$($string)'"
+        }
+    
+    }
+    
+    end {
+
+        Invoke-Expression ($cmdString -f $arrParams)
+        
+    }
+
+}#END: function Convert-ArrayToPath {}
