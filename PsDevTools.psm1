f421f5cbb7f7aa107b5a7ce85ccc14d4f02711d1
Tony Pagliaro <Tony@Pagliaro.co>
2021-09-22 16:52:49 -0400

    Update PSD1 (remove variables)

--- PsDevTools.psm1
+++ PsDevTools.psm1
@@ -2,4 +2,5 @@ $Functions = Get-ChildItem $PSScriptRoot\*.ps1
 Foreach ($File in $Functions) {
     . $File.FullName
 }
+Remove-Variable -Name 'Functions','File' -Force -ea 0
 Export-ModuleMember -Function * -Alias * -Variable *
