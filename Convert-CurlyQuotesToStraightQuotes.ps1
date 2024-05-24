75d007bda86b9b36bd443e351d9a6fc3ad62acc0
Tony Pagliaro <tony@pagliaro.co>
2024-05-24 13:46:29 -0400

    New function to handle fancy curly quotes

---
+++ Convert-CurlyQuotesToStraightQuotes.ps1
@@ -0,0 +1,35 @@
+﻿function Convert-CurlyQuotesToStraightQuotes {
+    [CmdletBinding()]
+    [Alias('curly')]
+    param(
+
+        # String line(s) of text with or without curly quotes
+        [Parameter(
+            ValueFromPipeline,
+            HelpMessage='line(s) of text with or without curly quotes',
+            Position=0
+        )]
+        [ValidateNotNull()]
+        [string[]]
+        $String
+
+    )#END: param
+
+    begin {
+
+        $fancySingleQuotes = "[\u2019\u2018]"
+        $fancyDoubleQuotes = "[\u201C\u201D]|&quot;"
+
+    }#END: begin
+
+    process {
+
+        foreach ($item in $String) {
+
+            $item -replace $fancySingleQuotes,"'" -replace $fancyDoubleQuotes,'"'
+
+        }#END: foreach ($item in $String)
+
+    }#END process
+
+}#END: function Convert-CurlyQuotesToStraightQuotes
